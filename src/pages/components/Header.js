1091aa29ffecd812fcfecd5adf4d1d5eeb20d0f2
avvncor <amaan.imtiyaz@gmail.com>
2021-06-14 01:06:34 +0530

    pwa

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -21,7 +21,7 @@ function Header() {
 
                 <h1 className="big-title translate" data-speed="0.1">Care.</h1>
 
-            <img src={Person}    className="person    translate"  data-speed="-0.25" alt="" />
+            <img src={Person}    className="person    translate"  data-speed="-0.1" alt="" />
             <img src={Mountain1} className="mountain1 translate"  data-speed="0.2" alt="" />
             <img src={Mountain2} className="mountain2 translate"  data-speed="0.4" alt="" />
             <img src={Mountain3} className="mountain3 translate"  data-speed="0.3" alt="" />
